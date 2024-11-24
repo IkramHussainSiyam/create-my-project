ae6ced0e9f32d938dabf2ffb4796770020d3e734
IkramHussainSiyam <ikramhussaindev404@gmail.com>
2024-11-24 15:13:20 +0600

    refactor: updated jsconfig & added jsdoc type checking.

--- templates/NextJS/JavaScript/src/lib/toast.js
+++ templates/NextJS/JavaScript/src/lib/toast.js
@@ -1,12 +1,19 @@
 import { toast } from "sonner";
 
 /**
+ * Function for showing `sonner` toasts
  * @typedef {'success' | 'error' | 'info' | 'warning' | 'loading'} Type
  * @typedef {'top-left' | 'top-right' | 'bottom-left' | 'bottom-right' | 'top-center' | 'bottom-center'} Position
+ * 
+ * @typedef {Object} Options
+ * @property {string | number} [id]
+ * @property {Position} [position]
+ * @property {number} [duration]
+ * @property {string} [description]
  *
  * @param {Type} type
  * @param {string} message
- * @param {{ position?: Position, id?: string | number, duration?: number, description?: string}} options
+ * @param {Options} [options]
  */
 
 export default function showToast(type, message, options) {
